d56234d7d7017d413fdc33b2ec282d13e68e3ab8
James Ko <jamesqko@gmail.com>
2020-02-24 12:22:53 -0500

    Initial commit

---
+++ client/app.py
@@ -0,0 +1,15 @@
+from urllib.parse import urljoin
+
+from flask import Flask
+import requests
+
+API_ROOT = 'http://127.0.0.1:5000/api/'
+app = Flask(__name__)
+
+def get(endpoint):
+    return requests.get(urljoin(API_ROOT, endpoint))
+
+@app.route('/<subreddit>')
+def index(subreddit):
+    top_articles = get(f'/top/{subreddit}')
+    return render_template('subreddit.html', articles=top_articles)
