e652390258e2bb844e7a811656d996bf0311f59f
James Ko <jamesqko@gmail.com>
2020-02-24 18:47:33 -0500

    Fix response parsing

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ API_ROOT = 'https://cisco-webapp-api.herokuapp.com/api/'
 app = Flask(__name__)
 
 def api_get(endpoint):
-    return requests.get(urljoin(API_ROOT, endpoint))
+    response = requests.get(urljoin(API_ROOT, endpoint))
+    return response.json()
 
 @app.route('/<subreddit>')
 def subreddit(subreddit):
-    top_articles = api_get(f'/top/{subreddit}')
+    top_articles = api_get(f'/top/{subreddit}')['data']
     return render_template('subreddit.html', subreddit=subreddit, articles=top_articles)
