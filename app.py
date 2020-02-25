eed89f8ed9a341913aa1cc6867ad261569bf0733
James Ko <jamesqko@gmail.com>
2020-02-24 19:02:07 -0500

    Logging

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
+import logging
 from urllib.parse import urljoin
 
 from flask import Flask, render_template
 import requests
 
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(message)s')
+
 API_ROOT = 'https://cisco-webapp-api.herokuapp.com/api/'
 app = Flask(__name__)
 
 def api_get(endpoint):
-    response = requests.get(urljoin(API_ROOT, endpoint))
+    url = urljoin(API_ROOT, endpoint)
+    logging.debug('Making API request to %s', url)
+    response = requests.get(url)
+    logging.debug('Response: %s', repr(response.text))
     return response.json()
 
 @app.route('/<subreddit>')
