7be58d7afa87f10bd3d770ed613544a8087bf4fd
James Ko <jamesqko@gmail.com>
2020-02-24 20:42:12 -0500

    Finishing touches

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import logging
-from urllib.parse import urljoin
+from urllib.parse import urlencode, urljoin
 
 from flask import Flask, render_template
 import requests
@@ -10,8 +10,9 @@ logging.basicConfig(level=logging.DEBUG,
 API_ROOT = 'https://cisco-webapp-api.herokuapp.com/'
 app = Flask(__name__)
 
-def api_get(endpoint):
+def api_get(endpoint, **kw):
     url = urljoin(API_ROOT, endpoint)
+    url += ('?' + urlencode(kw))
     logging.debug('Making API request to %s', url)
     response = requests.get(url)
     logging.debug('Response: %s', repr(response.text))
@@ -19,5 +20,5 @@ def api_get(endpoint):
 
 @app.route('/top/<subreddit>')
 def top(subreddit):
-    top_articles = api_get(f'/top/{subreddit}')['data']
+    top_articles = api_get(f'/top/{subreddit}', limit=25)['data']
     return render_template('top.html', subreddit=subreddit, articles=top_articles)
